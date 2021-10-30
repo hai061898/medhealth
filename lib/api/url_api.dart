b6f3bc6f52c8058bbde3d405b85a30ef7b270bb0
hai061898 <truonghaiauto98@gmail.com>
2021-10-30 21:28:35 +0700

    login

--- lib/api/url_api.dart
+++ lib/api/url_api.dart
@@ -1,5 +1,9 @@
 class BASEURL {
    static String ipAddress = "127.0.0.1";
    static String apiRegister = "http://$ipAddress/medhealth_db/register_api.php";
+   static String apiLogin = "http://$ipAddress/medhealth_db/login_api.php";
 
+
+
+   
 }
